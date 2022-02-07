6b1d6ddaea13b338583693a3a754ef7a20f81ab8
Bekbolot Tazhibaev <tabekg@gmail.com>
2022-02-07 12:37:18 +0600

    sort fix

--- src/components/ProductCard/FilterBlock.jsx
+++ src/components/ProductCard/FilterBlock.jsx
@@ -5,31 +5,50 @@ import {
   Select,
   TextField,
 } from "@mui/material";
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { getProducts } from "../../actions/AdminAction";
 
-const FilterBlock = (props) => {
+const FilterBlock = () => {
   const search = new URLSearchParams(window.location.search);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const [searchValue, setSearchValue] = useState(search.get("q") || "");
   const [categoriesValue, setCategoriesValue] = useState(
-    search.get("categories") || ""
+    search.get("categories") || `_${search.get('_order') || ''}` || ""
   );
 
-  const filterProdusts = (key, value) => {
-    setCategoriesValue(value);
-    if (value === "high to low" || value === "low to hight") {
-      props.setSorting(value);
+  useEffect(() => {
+    if (categoriesValue && !categoriesValue.startsWith('_')) {
+      search.set('categories', categoriesValue);
+      search.delete('_sort');
+      search.delete('_order');
+    } else if (categoriesValue && categoriesValue !== '_') {
+      search.delete('categories');
+      search.set('_sort', 'price');
+      search.set('_order', categoriesValue.slice(1));
     } else {
-      search.set(key, value);
-      let newPath = `${window.location.pathname}?${search.toString()}`;
-      navigate(newPath);
-      setSearchValue(search.get("q") || "");
-      dispatch(getProducts());
+      search.delete('categories');
+    }
+
+    if (searchValue) {
+      search.set('q', searchValue);
+    } else {
+      search.delete('q');
+    }
+
+    navigate(`${window.location.pathname}?${search.toString()}`);
+    setSearchValue(search.get("q") || "");
+    dispatch(getProducts());
+  }, [categoriesValue, searchValue]);
+
+  const filterProducts = (key, value) => {
+    if (key === 'categories') {
+      setCategoriesValue(value);
+    } else {
+      setSearchValue(value);
     }
   };
 
@@ -43,27 +62,27 @@ const FilterBlock = (props) => {
     >
       <TextField
         value={searchValue}
-        onChange={(e) => filterProdusts("q", e.target.value)}
+        onChange={(e) => filterProducts("q", e.target.value)}
         variant="standard"
         label="Живой поиск..."
       />
       <FormControl variant="standard" sx={{ minWidth: 150 }}>
         <InputLabel id="categories-select">Sorting</InputLabel>
         <Select
-          value={categoriesValue}
-          onChange={(e) => filterProdusts("categories", e.target.value)}
+          value={categoriesValue === '_' ? '' : categoriesValue}
+          onChange={(e) => filterProducts("categories", e.target.value)}
           labelId="categories-select"
           label="Sorting"
         >
-          <MenuItem value="">Default sorting</MenuItem>
+          <MenuItem value={''}>Default sorting</MenuItem>
           <MenuItem value="pizza">Пицца</MenuItem>
           <MenuItem value="combo">Комбо</MenuItem>
           <MenuItem value="snacks">Закуски</MenuItem>
           <MenuItem value="desserts">Десерты</MenuItem>
           <MenuItem value="beverages">Напитки</MenuItem>
           {}
-          <MenuItem value="low to hight">Sort by price: low to high</MenuItem>
-          <MenuItem value="high to low">Sort by price: high to low</MenuItem>
+          <MenuItem value="_asc">Sort by price: low to high</MenuItem>
+          <MenuItem value="_desc">Sort by price: high to low</MenuItem>
         </Select>
       </FormControl>
     </div>
